bed65a937b2238922059bb28c04b58d1ec1eac59
swaingotnochill <swainroshan001@gmail.com>
2024-02-13 15:08:28 +0530

    feat: adds a tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,66 @@
+use serde_json::Result;
 use std::collections::HashMap;
 use std::fs::{self, File};
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use xml::reader::{EventReader, XmlEvent};
+#[derive(Debug)]
+struct Lexer<'a> {
+    content: &'a [char],
+}
+
+impl<'a> Lexer<'a> {
+    fn new(content: &'a [char]) -> Self {
+        Self { content }
+    }
+    fn trim_left(&mut self) {
+        while self.content.len() > 0 && self.content[0].is_whitespace() {
+            self.content = &self.content[1..];
+        }
+    }
+
+    fn chop(&mut self, n: usize) -> &'a [char] {
+        let token = &self.content[0..n];
+        self.content = &self.content[n..];
+        return &token;
+    }
+
+    fn chop_while<P>(&mut self, mut predicate: P) -> &'a [char]
+    where
+        P: FnMut(&char) -> bool,
+    {
+        let mut idx = 0;
+        while idx < self.content.len() && predicate(&self.content[idx]) {
+            idx += 1;
+        }
+        self.chop(idx)
+    }
+
+    fn next_token(&mut self) -> Option<&'a [char]> {
+        // trim whitespaces from left.
+        self.trim_left();
+        if self.content.len() == 0 {
+            return None;
+        }
+
+        if self.content[0].is_numeric() {
+            return Some(self.chop_while(|idx| idx.is_numeric()));
+        }
+
+        if self.content[0].is_alphabetic() {
+            return Some(self.chop_while(|idx| idx.is_alphabetic()));
+        }
+        return Some(self.chop(1));
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = &'a [char];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_token()
+    }
+}
 
 fn index_document(_doc_content: &str) -> HashMap<String, usize> {
     todo!("not implemented");
@@ -15,20 +73,65 @@ fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     for event in er.into_iter() {
         if let XmlEvent::Characters(text) = event.expect("TODO") {
             content.push_str(&text);
+            content.push_str(" ");
         }
     }
     Ok(content)
 }
 
-fn main() -> io::Result<()> {
-    let all_documents = HashMap::<Path, HashMap<String, usize>>::new()
+type TermFreq = HashMap<String, usize>;
+type TermFreqIndex = HashMap<PathBuf, TermFreq>;
 
+fn main() -> io::Result<()> {
+    let index_path = "index.json";
+    let index_file = File::open(index_path)?;
+    println!("Reading {index_path} index file...");
+    let tf_index: TermFreqIndex = serde_json::from_reader(index_file).expect("serde does not fail");
+    println!(
+        "{index_path} contains {count} files",
+        count = tf_index.len()
+    );
+    Ok(())
+}
+
+fn main2() -> io::Result<()> {
+    // let all_documents = HashMap::<Path, HashMap<String, usize>>::new();
     let dir_path = "docs.gl/gl4";
     let dir = fs::read_dir(dir_path)?;
+    let mut tf_index = TermFreqIndex::new();
     for file in dir {
         let file_path = file?.path();
-        let content = read_entire_xml_file(&file_path);
-        println!("{file_path:?} ==> {size}", size = content?.len());
+
+        println!("Indexing {file_path:?}...");
+
+        let content = read_entire_xml_file(&file_path)?
+            .chars()
+            .collect::<Vec<_>>();
+
+        let mut tf = TermFreq::new();
+
+        for token in Lexer::new(&content) {
+            let term = token
+                .iter()
+                .map(|x| x.to_ascii_uppercase())
+                .collect::<String>();
+            if let Some(freq) = tf.get_mut(&term) {
+                *freq += 1;
+            } else {
+                tf.insert(term, 1);
+            }
+        }
+        let mut stats = tf.iter().collect::<Vec<_>>();
+        stats.sort_by_key(|(_, f)| *f);
+        stats.reverse();
+
+        tf_index.insert(file_path, tf);
     }
+
+    let index_path = "index.json";
+    println!("Saving {index_path}...");
+    let index_file = File::create(index_path)?;
+    serde_json::to_writer(index_file, &tf_index).expect("serde works fine.");
+
     Ok(())
 }
